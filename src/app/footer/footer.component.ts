246804beb74d6bf661a054373e29a9d9b5d5e57f
brchen777 <eric790620@gmail.com>
2017-06-10 14:05:07 +0800

    任務 10：套用 @Input() 裝飾器 (Decorator) - 進階1

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, OnChanges {
 
-  @Input() todos: any[] = [];
+  @Input('todos') myTodos: any[] = [];
+  moreThanFive: boolean = false;
+
+  ngOnChanges() {
+    if (this.myTodos.length >= 5) {
+      this.moreThanFive = true;
+    }
+  }
 
   constructor() { }
 
